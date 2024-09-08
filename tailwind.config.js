8a35d5057b67f898a39367de4dddae49a4d0b320
Charlie Miller <charliemiller1016@gmail.com>
2024-09-08 17:12:20 -0400

    Tailwind Configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "dropShadow": {
+        "card": "0px 2px 8px #000421"
+      },
+      "spacing": {
+        "card": "-6.7rem"
+      }
+    },
+    colors: {
+      "light-purple": "#D48AE0",
+      "medium-purple": "#7B4F82",
+      "medium-dark-purple": "#4D3052",
+      "dark-purple": "#000421",
+    }
   },
   plugins: [],
 };
