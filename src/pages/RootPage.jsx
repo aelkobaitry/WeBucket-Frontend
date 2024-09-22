0c02e9d28363bb3fde95a92c525dae07ec6f2452
Adam Elkobaitry <todoomz@gmail.com>
2024-09-22 17:41:02 -0400

    Add footer component

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import logo from "../assets/Logo.svg";
+import Footer from "../components/Footer";
 
 const RootPage = () => {
   return (
-    <div className="relative z-10 flex items-center">
-      <img src={logo} className="w-36" alt="WeBucket Logo" />
-      <div className="text-[#A778AF] font-bold text-4xl">
-        Welcome to WeBucket
+    <>
+      <div className="relative z-10 flex flex-col items-center justify-center mt-56 mb-96">
+        <img src={logo} className="w-36" alt="WeBucket Logo" />
+        <div className="text-[#A778AF] font-bold text-4xl">
+          Welcome to WeBucket
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
